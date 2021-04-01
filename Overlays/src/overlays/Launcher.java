239e00038ddd4887b2b8abc7e9d151f03ed0424e
templier william <william.templier@etu.univ-grenoble-alpes.fr>
2021-04-01 16:31:48 +0200

    classes

---
+++ Overlays/src/overlays/Launcher.java
@@ -0,0 +1,16 @@
+package overlays;
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!" + args[0]);
+
+        // get matrix
+
+        // create RoutingNode
+
+        // create N Virtual node
+
+        // ....
+
+    }
+}
---
+++ Overlays/src/overlays/VirtualNode.java
@@ -0,0 +1,5 @@
+package overlays;
+
+public class VirtualNode {
+    // virtual
+}
